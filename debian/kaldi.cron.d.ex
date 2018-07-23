e633315fd0af470c8b1d4b8170a763e012611013
Marc Püls <mpuels@gmail.com>
2018-07-23 09:29:47 +0200

    First successfull build of binary package of libkaldi

--- debian/kaldi.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the kaldi package
-#
-0 4	* * *	root	[ -x /usr/bin/kaldi_maintenance ] && /usr/bin/kaldi_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(kaldi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="kaldi" command="/usr/bin/kaldi"
